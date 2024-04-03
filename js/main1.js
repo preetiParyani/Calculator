df9b4f03dded16b50196846a7dd2ff0f6bec4bb7
Preeti Paryani <preetiparyani29@gmail.com>
2024-04-03 13:41:31 +0530

    Added all the required files

---
+++ js/main1.js
@@ -0,0 +1,70 @@
+// NOTE: 
+// This is the final source code file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
+
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2)
+    }
+  
+    return result
+  }
+  
+  const calculator = document.querySelector('.calculator')
+  const display = calculator.querySelector('.calculatorDisplay')
+  const keys = calculator.querySelector('.calculatorKeys')
+  
+  keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+      const key = e.target
+      const action = key.dataset.action
+      const keyContent = key.textContent
+      const displayedNum = display.textContent
+      const previousKeyType = calculator.dataset.previousKeyType
+  
+      Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'))
+  
+      if (!action) {
+        if (displayedNum === '0' || previousKeyType === 'operator') {
+          display.textContent = keyContent
+        } else {
+          display.textContent = displayedNum + keyContent
+        }
+      }
+  
+      if (action === 'decimal') {
+        display.textContent = displayedNum + '.'
+      }
+  
+      if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        key.classList.add('is-depressed')
+        calculator.dataset.previousKeyType = 'operator'
+        calculator.dataset.firstValue = displayedNum
+        calculator.dataset.operator = action
+      }
+  
+      if (action === 'clear') {
+        console.log('clear key!')
+      }
+  
+      if (action === 'calculate') {
+        const firstValue = calculator.dataset.firstValue
+        const operator = calculator.dataset.operator
+        const secondValue = displayedNum
+  
+        display.textContent = calculate(firstValue, operator, secondValue)
+      }
+    }
+  })
